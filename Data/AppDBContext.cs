7972a8e9802e68dd6ba2d625d5800e852fcad9b8
Oleksandr Shevchuk <oleksandr.shevchuk222@gmail.com>
2024-04-16 22:08:38 +0300

    Added Data

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VotingSystem.Models;
+
+namespace VotingSystem.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions options) : base(options) { }
+        public DbSet<Candidate> Candidate { get; set; }
+        public DbSet<VoteCastingInfo> VoteCastingInfo { get; set; }
+        public DbSet<Voter> Voter { get; set; }
+    }
+}
---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VotingSystem.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
